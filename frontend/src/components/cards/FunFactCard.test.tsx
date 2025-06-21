6ba132759a52833d6f14fdde8660a010bb83c222
YoungSlim971 <alan.ee971@gmail.com>
2025-06-21 17:51:48 +0200

    fix frontend tests (#32)

--- frontend/src/components/cards/FunFactCard.test.tsx
+++ frontend/src/components/cards/FunFactCard.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { FunFactCard } from './FunFactCard';
 
 global.fetch = jest.fn(() =>
--- frontend/src/components/cards/WeatherClockCard.test.tsx
+++ frontend/src/components/cards/WeatherClockCard.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { WeatherClockCard } from './WeatherClockCard';
 
 global.fetch = jest
