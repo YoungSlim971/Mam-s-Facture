9396ca8ce1e834c4bb48b7d6f39d10d026c78fee
YoungSlim971 <alan.ee971@gmail.com>
2025-06-23 00:46:20 +0200

    feat(app): check internet connection on startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 import './index.css'
 import App from './App.tsx'
+import { checkInternetConnection } from './utils/network.ts'
+
+checkInternetConnection()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
