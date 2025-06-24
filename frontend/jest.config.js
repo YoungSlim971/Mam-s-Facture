6b6fc75e76985302bf7e9ea8e731c2bb0c6f8de6
YoungSlim971 <alan.ee971@gmail.com>
2025-06-24 07:14:17 +0200

    chore(test): fix ts-jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,11 +6,14 @@ export default {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.app.json',
-      useESM: true,
-    },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.app.json',
+        useESM: true,
+      },
+    ],
   },
   // Babel config is removed as it wasn't solving the import.meta issue correctly
   // and we are now mocking the modules that use import.meta.env.
