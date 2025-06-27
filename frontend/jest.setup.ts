db25dc9ba5b03d2ff8f47d1aa2a6ae78c7be73b8
YoungSlim971 <alan.ee971@gmail.com>
2025-06-27 02:33:19 +0200

    Fix InvoicePieChart to display local chart

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,3 +1,15 @@
 beforeAll(() => {
   localStorage.setItem('apiToken', 'test-token');
 });
+
+// JSDOM ne gère pas ResizeObserver, nécessaire pour Recharts
+beforeAll(() => {
+  if (typeof window !== 'undefined' && !('ResizeObserver' in window)) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).ResizeObserver = class {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    };
+  }
+});
